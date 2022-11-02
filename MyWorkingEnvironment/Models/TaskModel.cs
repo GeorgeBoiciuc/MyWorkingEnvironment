083881cb80ae7fd169d51419bec66ebc6a3c1451
GeorgeBoiciuc <boiciuc.georgem@gmail.com>
2022-11-02 23:55:34 +0200

    Models done -- TaskModel

---
+++ MyWorkingEnvironment/Models/TaskModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MyWorkingEnvironment.Models
+{
+    public class TaskModel
+    {
+        public Guid IdTask { get; set; }
+        public string Title { get; set; } = null!;
+        public string Priority { get; set; } = null!;
+        public string? Description { get; set; }
+    }
+}
